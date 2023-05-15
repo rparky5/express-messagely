97faaac6534a293df100e39a4a0a1bfe6dfcc27e
Ryan Park <ryrypark@gmail.com>
2023-05-15 15:53:21 -0700

    finish user model and start testing

--- models/user.js
+++ models/user.js
@@ -14,15 +14,17 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+    //TODO: hash password and store it
     const result = await db.query(
       `INSERT INTO users (username,
                           password,
                           first_name,
                           last_name,
                           phone,
-                          join_at)
+                          join_at,
+                          last_login_at)
          VALUES
-           ($1, $2, $3, $4, $5, current_timestamp)
+           ($1, $2, $3, $4, $5, current_timestamp, current_timestamp)
          RETURNING username, password, first_name, last_name, phone`,
     [username, password, first_name, last_name, phone]);
 
@@ -104,14 +106,36 @@ class User {
 
   static async messagesFrom(username) {
     const results = await db.query(
-      `SELECT id, to_username AS to_user, body, sent_at, read_at
-       FROM users AS u
-       WHERE u.username = $1
-       JOIN messages AS m
-       ON m.from_username = u.username`,
-       [username]);
+      `SELECT m.id AS id,
+              m.body AS body,
+              m.sent_at AS sent_at,
+              m.read_at AS read_at,
+              t.username AS to_username,
+              t.first_name AS to_first_name,
+              t.last_name AS to_last_name,
+              t.phone AS to_phone
+      FROM messages AS m
+      JOIN users AS f ON m.from_username = f.username
+      JOIN users AS t ON m.to_username = t.username
+      WHERE f.username = $1`,
+      [username]);
 
-    const toUsers = results.rows.map(user => user.to_user)
+    const messages = results.rows.map(message => {
+      return {
+        id: message.id,
+        to_user: {
+          username: message.to_username,
+          first_name: message.to_first_name,
+          last_name: message.to_last_name,
+          phone: message.to_phone
+        },
+        body: message.body,
+        sent_at: message.sent_at,
+        read_at: message.read_at
+      }
+    });
+
+    return messages
   }
 
   /** Return messages to this user.
@@ -123,6 +147,37 @@ class User {
    */
 
   static async messagesTo(username) {
+    const results = await db.query(
+      `SELECT m.id,
+              m.body,
+              m.sent_at,
+              m.read_at,
+              f.username,
+              f.first_name,
+              f.last_name,
+              f.phone
+      FROM messages AS m
+      JOIN users AS t ON m.to_username = t.username
+      JOIN users AS f ON m.from_username = f.username
+      WHERE t.username = $1`,
+      [username]);
+
+    const messages = results.rows.map(message => {
+      return {
+        id: message["m.id"],
+        from_user: {
+          username: message["f.username"],
+          first_name: message["f.first_name"],
+          last_name: message["f.last_name"],
+          phone: message["f.phone"]
+        },
+        body: message["m.body"],
+        sent_at: message["m.sent_at"],
+        read_at: message["m.read_at"]
+      }
+    });
+
+    return messages
   }
 }
 
